fd13a54e89a96f35c54279e21e0ccef13d62d979
Tim Ittermann <tim.ittermann@gmail.com>
2024-03-21 17:38:42 +0100

    WORKAROUND: Better Text alignment

--- Layouting/TextElement.cs
+++ Layouting/TextElement.cs
@@ -12,7 +12,7 @@ public class TextElement(string content, SKPaint paint) : Element
     public override void Draw(DrawContext drawContext)
     {
         var point = drawContext.StartPoint;
-        point.Y += paint.TextSize;
+        point.Y += paint.TextSize * 0.9f;
 
         drawContext.Canvas.DrawText(content, point, paint);
     }
