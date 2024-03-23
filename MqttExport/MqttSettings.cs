90ca5fb8db4fbbb6e5aaa3e73e6e1e1c9cc991f5
Tim Ittermann <tim.ittermann@gmail.com>
2024-03-23 19:41:10 +0100

    Reconfigure Settings

--- MqttExport/MqttSettings.cs
+++ MqttExport/MqttSettings.cs
@@ -2,7 +2,8 @@ namespace DisplayUtil.MqttExport;
 
 public record MqttSettings
 {
-    public string Uri { get; set; } = null!;
-    public string User { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string? Uri { get; init; } = null!;
+    public string? User { get; init; } = null!;
+    public string? Password { get; init; } = null!;
+    public string? ScreenDetectTemplate { get; init; }
 }
