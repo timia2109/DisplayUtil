f91a9712a3920f0582881c56981458f165bd3a29
Tim Ittermann <tim.ittermann@gmail.com>
2024-08-29 15:12:35 +0200

    Increase Mapping Providers

--- DisplayUtil.Scripting/PropertyObjects/Mapping/MappingProvider.cs
+++ DisplayUtil.Scripting/PropertyObjects/Mapping/MappingProvider.cs
@@ -5,14 +5,75 @@ using Jint.Runtime;
 
 namespace DisplayUtil.Scripting.PropertyObjects.Mapping;
 
-public class MappingProvider
+public static class MappingFactory
 {
 
+    public static PropertyMapping ForMemberInfo(MemberInfo member)
+    {
+        return member switch
+        {
+            FieldInfo field => ForFieldInfo(field),
+            PropertyInfo property => ForPropertyInfo(property),
+            _ => throw new ArgumentException("MemberInfo must be a FieldInfo or PropertyInfo")
+        };
+    }
+
+    private static PropertyMapping ForPropertyInfo(PropertyInfo property)
+    {
+        return new PropertyMapping()
+        {
+            PropertyName = property.Name,
+            Mapping = ForType(property.PropertyType),
+            Member = property
+        };
+    }
+
+    private static PropertyMapping ForFieldInfo(FieldInfo field)
+    {
+        return new PropertyMapping()
+        {
+            PropertyName = field.Name,
+            Mapping = ForType(field.FieldType),
+            Member = field
+        };
+    }
+
+    public static IMapping ForType(Type type)
+    {
+        switch (Type.GetTypeCode(type))
+        {
+            case TypeCode.Boolean:
+                return new BooleanMapping();
+            case TypeCode.String:
+                return new StringMapping();
+            case TypeCode.UInt64:
+            case TypeCode.Int64:
+            case TypeCode.UInt32:
+            case TypeCode.Int32:
+            case TypeCode.UInt16:
+            case TypeCode.Int16:
+            case TypeCode.Byte:
+            case TypeCode.SByte:
+            case TypeCode.Decimal:
+            case TypeCode.Double:
+            case TypeCode.Single:
+                return new NumberMapping(type);
+            case TypeCode.Object:
+                if (type.IsEnum)
+                    return new EnumMapping(type);
+                if (type.IsArray)
+                    return new ArrayMapping(type.GetElementType()!, ForType(type.GetElementType()!));
+                return new ObjectMapping(type, GetPropertyMappings(type));
+        }
+    }
 }
 
-public sealed class TypeException(Types expected, Types actual)
-    : Exception($"Expected {expected} but got {actual}");
-
+public sealed class TypeException(string expected, Types actual)
+    : Exception($"Expected {expected} but got {actual}")
+{
+    public TypeException(Types expected, Types actual)
+        : this(expected.ToString(), actual) { }
+}
 
 /// <summary>
 /// Represents a mapping between a JsValue and a .NET object
@@ -32,7 +93,6 @@ internal abstract class BaseMapping : IMapping
 {
     public object Map(JsValue jsValue)
     {
-
         if (jsValue.Type != ExpectedType)
             throw new TypeException(ExpectedType, jsValue.Type);
 
@@ -44,13 +104,14 @@ internal abstract class BaseMapping : IMapping
     protected abstract object MapInternal(JsValue jsValue);
 }
 
-internal class NumberMapping : BaseMapping
+internal class NumberMapping(Type expectedType) : BaseMapping
 {
     protected override Types ExpectedType => Types.Number;
 
     protected override object MapInternal(JsValue jsValue)
     {
-        return jsValue.AsNumber();
+        var d = jsValue.AsNumber();
+        return Convert.ChangeType(d, expectedType);
     }
 }
 
@@ -74,9 +135,38 @@ internal class BooleanMapping : BaseMapping
     }
 }
 
+internal class EnumMapping(Type enumType) : BaseMapping
+{
+    protected override Types ExpectedType => Types.String;
+
+    protected override object MapInternal(JsValue jsValue)
+    {
+        return Enum.Parse(enumType, jsValue.AsString());
+    }
+}
+
+internal class ArrayMapping(Type itemType, IMapping itemMapping) : IMapping
+{
+    public object Map(JsValue jsValue)
+    {
+        if (!jsValue.IsArray())
+            throw new TypeException("Array", jsValue.Type);
+
+        var arr = jsValue.AsArray();
+        var items = Array.CreateInstance(itemType, arr.Length);
+
+        for (var i = 0; i < arr.Length; i++)
+            items.SetValue(itemMapping.Map(arr[i]), i);
+
+        return items;
+    }
+}
+
+/// <summary>
+/// Wraps a mapping for a property
+/// </summary>
 internal class PropertyMapping
 {
-
     public required string PropertyName { get; init; }
     public required IMapping Mapping { get; init; }
     public required MemberInfo Member { get; init; }
@@ -96,6 +186,11 @@ internal class PropertyMapping
     }
 }
 
+/// <summary>
+/// Wraps a Mapping for a complete Object
+/// </summary>
+/// <typeparam name="TType">Type of target</typeparam>
+/// <param name="children">Properties</param>
 internal class ObjectMapping<TType>(PropertyMapping[] children) : BaseMapping
     where TType : new()
 {
@@ -103,6 +198,12 @@ internal class ObjectMapping<TType>(PropertyMapping[] children) : BaseMapping
 
     protected override object MapInternal(JsValue jsValue)
     {
+        var plainObject = jsValue.ToObject();
+
+        if (plainObject is not null &&
+            typeof(TType).IsAssignableFrom(plainObject.GetType()))
+            return plainObject;
+
         var jsObject = (JsObject)jsValue;
         var obj = new TType();
 
