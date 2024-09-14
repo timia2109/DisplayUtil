6d450a503abc0c5d4a07f3e5ead80b7875f76df1
Tim Ittermann <tim.ittermann@gmail.com>
2024-09-14 11:42:36 +0200

    feat: support binary transport

--- DisplayUtil/EspUtilities/EspUtilitiesInitExtension.cs
+++ DisplayUtil/EspUtilities/EspUtilitiesInitExtension.cs
@@ -4,6 +4,8 @@ namespace DisplayUtil.EspUtilities;
 
 public static class EspUtilitiesInitExtension
 {
+    private const string BinaryMediaType = "application/octet-stream";
+
     public const string CompressedImageRoute = "/esp/{providerId}",
                         PlainImageRoute = "/esp/bits/{providerId}";
 
@@ -17,10 +19,17 @@ public static class EspUtilitiesInitExtension
     {
         app.MapGet(CompressedImageRoute, async (string providerId, HttpContext ctx, EspImageProvider espProvider) =>
         {
+            var accept = ctx.Request.Headers.Accept;
             var (data, size) = await espProvider.GetAsRunLengthAsync(providerId);
-            var base64 = Convert.ToBase64String(data);
             ctx.Response.Headers.Append("X-Width", size.Width.ToString());
             ctx.Response.Headers.Append("X-Height", size.Height.ToString());
+
+            if (accept.Contains(BinaryMediaType))
+            {
+                return Results.Bytes(data, BinaryMediaType);
+            }
+
+            var base64 = Convert.ToBase64String(data);
             return Results.Text(base64, "text/plain", Encoding.ASCII);
         })
             .WithName("Get ESP Image")
@@ -28,10 +37,17 @@ public static class EspUtilitiesInitExtension
 
         app.MapGet(PlainImageRoute, async (string providerId, HttpContext ctx, EspImageProvider espProvider) =>
         {
+            var accept = ctx.Request.Headers.Accept;
             var (data, size) = await espProvider.GetAsPlainBytesAsync(providerId);
-            var base64 = Convert.ToBase64String(data);
             ctx.Response.Headers.Append("X-Width", size.Width.ToString());
             ctx.Response.Headers.Append("X-Height", size.Height.ToString());
+
+            if (accept.Contains(BinaryMediaType))
+            {
+                return Results.Bytes(data, BinaryMediaType);
+            }
+
+            var base64 = Convert.ToBase64String(data);
             return Results.Text(base64, "text/plain", Encoding.ASCII);
         })
             .WithName("Get ESP Bit Image")
