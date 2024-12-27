c4ec21805338b74a7c40fee3c1df5434af7829df
Tim Ittermann <tim.ittermann@gmail.com>
2024-12-28 00:00:07 +0100

    feat: add first ESP Utilities

---
+++ DisplayUtil.Infrastructure/EspUtilities/EspImageProvider.cs
@@ -0,0 +1,74 @@
+using System.Diagnostics;
+using DisplayUtil.Scenes;
+using Microsoft.Extensions.Logging;
+using SkiaSharp;
+
+namespace DisplayUtil.Infrastructure.EspUtilities;
+
+/// <summary>
+/// Responsible to provide the images in a form for the ESP.
+/// Scoped
+/// </summary>
+public partial class EspImageProvider(
+    ILogger<EspImageProvider> logger,
+    ScreenRepository screenRepository
+)
+{
+    private readonly ILogger _logger = logger;
+
+    /// <summary>
+    /// Gets the image as plain true / false Bytes
+    /// </summary>
+    /// <param name="providerId">Id of the provider</param>
+    /// <returns>Byte Array</returns>
+    public async Task<(byte[], SKSize)> GetAsPlainBytesAsync(string providerId)
+    {
+        var stopwatch = new Stopwatch();
+        LogRender(providerId);
+        stopwatch.Start();
+
+        //Render
+        using var image = await screenRepository.GetImageAsync(providerId);
+        stopwatch.Stop();
+        var elapsed = stopwatch.ElapsedMilliseconds;
+        LogRenderTime(elapsed);
+
+        var binaryData = BinaryImageStreamCreator.GetImageStream(image);
+        LogPlainBytes(binaryData.Length);
+        return (binaryData, new SKSize(image.Width, image.Height));
+    }
+
+    /// <summary>
+    /// Gets the Image as RunLength Compressed
+    /// </summary>
+    /// <param name="providerId">Id of the provider</param>
+    /// <returns>Compressed Data</returns>
+    public async Task<(byte[], SKSize)> GetAsRunLengthAsync(string providerId)
+    {
+        var (plainBytes, size) = await GetAsPlainBytesAsync(providerId);
+
+        var runLengthEncoder = new RunLengthCompressor();
+        var compressedData = runLengthEncoder.WriteStream(plainBytes);
+
+        var compressedPercent = compressedData.Length / (float)plainBytes.Length;
+        LogCompressedBytes(
+            compressedData.Length,
+            Math.Round((1 - compressedPercent) * 100, 2)
+        );
+
+        return (compressedData, size);
+    }
+
+
+    [LoggerMessage(LogLevel.Information, "Render screen {providerId}")]
+    private partial void LogRender(string providerId);
+
+    [LoggerMessage(LogLevel.Information, "Rendering took {time} ms")]
+    private partial void LogRenderTime(long time);
+
+    [LoggerMessage(LogLevel.Information, "Plain binary takes {bytes} bytes")]
+    private partial void LogPlainBytes(int bytes);
+
+    [LoggerMessage(LogLevel.Information, "Compressing takes {bytes} bytes ({percent} % less)")]
+    private partial void LogCompressedBytes(int bytes, double percent);
+}
---
+++ DisplayUtil.Infrastructure/EspUtilities/EspUtilitiesInitExtension.cs
@@ -0,0 +1,74 @@
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DisplayUtil.Infrastructure.EspUtilities;
+
+public static class EspUtilitiesInitExtension
+{
+    private const string BinaryMediaType = "application/octet-stream";
+
+    public const string CompressedImageRoute = "/esp/{providerId}",
+                        PlainImageRoute = "/esp/bits/{providerId}";
+
+    /// <summary>
+    /// Add the ESP Utilities
+    /// </summary>
+    /// <param name="builder">Application Builder</param>
+    /// <returns>Application Builder</returns>
+    public static IHostApplicationBuilder AddEspUtilities(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<EspImageProvider>();
+        return builder;
+    }
+
+    /// <summary>
+    /// Maps the ESP Utilities
+    /// </summary>
+    /// <param name="app">Web App</param>
+    /// <returns>Web Application</returns>
+    public static WebApplication UseEspUtilities(this WebApplication app)
+    {
+        app.MapGet(CompressedImageRoute, async (string providerId, HttpContext ctx, EspImageProvider espProvider) =>
+        {
+            var accept = ctx.Request.Headers.Accept;
+            var (data, size) = await espProvider.GetAsRunLengthAsync(providerId);
+            ctx.Response.Headers.Append("X-Width", size.Width.ToString());
+            ctx.Response.Headers.Append("X-Height", size.Height.ToString());
+
+            if (accept.Contains(BinaryMediaType))
+            {
+                return Results.Bytes(data, BinaryMediaType);
+            }
+
+            var base64 = Convert.ToBase64String(data);
+            return Results.Text(base64, "text/plain", Encoding.ASCII);
+        })
+            .WithName("Get ESP Image")
+            .WithOpenApi();
+
+        app.MapGet(PlainImageRoute, async (string providerId, HttpContext ctx, EspImageProvider espProvider) =>
+        {
+            var accept = ctx.Request.Headers.Accept;
+            var (data, size) = await espProvider.GetAsPlainBytesAsync(providerId);
+            ctx.Response.Headers.Append("X-Width", size.Width.ToString());
+            ctx.Response.Headers.Append("X-Height", size.Height.ToString());
+
+            if (accept.Contains(BinaryMediaType))
+            {
+                return Results.Bytes(data, BinaryMediaType);
+            }
+
+            var base64 = Convert.ToBase64String(data);
+            return Results.Text(base64, "text/plain", Encoding.ASCII);
+        })
+            .WithName("Get ESP Bit Image")
+            .WithOpenApi();
+
+        return app;
+    }
+
+
+}
