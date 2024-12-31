79412d6e8e54d81c5170e8afbb2bd886a174056b
Tim Ittermann <tim.ittermann@gmail.com>
2024-12-31 17:38:37 +0100

    feat: add personal server

---
+++ DisplayUtil.Home/Program.cs
@@ -0,0 +1,38 @@
+using DisplayUtil.Home.Screens;
+using DisplayUtil.Infrastructure;
+using DisplayUtil.Infrastructure.EspUtilities;
+using DisplayUtil.Infrastructure.Providers.Font;
+using DisplayUtil.Infrastructure.Providers.Icons;
+using DisplayUtil.Infrastructure.Providers.Image;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true);
+
+builder.Services
+    .AddOpenApi()
+    .AddEspUtilities()
+    .AddScreenBuilder(d => d
+        .WithTextSize(45)
+        .WithFont("Roboto")
+        .WithIconHeight(45)
+    )
+    .AddFontProvider(builder.Configuration.GetSection("Fonts"))
+    .AddFolderIconProvider(builder.Configuration);
+
+builder.Services
+    .AddSingleImageProvider<ExampleScreen>("example");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapImagePreview();
+app.UseEspUtilities();
+
+app.Run();
---
+++ DisplayUtil.Home/Screens/ExampleScreen.cs
@@ -0,0 +1,26 @@
+using DisplayUtil.Building;
+using DisplayUtil.Infrastructure.Providers.Image;
+using DisplayUtil.Layouting;
+using SkiaSharp;
+
+namespace DisplayUtil.Home.Screens;
+
+public sealed class ExampleScreen(ScreenBuilder screenBuilder) : ISingleImageProvider
+{
+    public ValueTask<SKBitmap> GetImageAsync()
+    {
+        var builder = screenBuilder
+            .WithWidth(800)
+            .WithHeight(480);
+
+        builder.UseFlexbox()
+            .WithDirection(FlexDirection.Vertical)
+            .WithJustifyContent(JustifyContent.Between)
+
+            .WithIcon("fa:home", 80)
+            .WithText("Hello World!")
+            .WithText("This is an example screen");
+
+        return ValueTask.FromResult(builder.Draw());
+    }
+}
