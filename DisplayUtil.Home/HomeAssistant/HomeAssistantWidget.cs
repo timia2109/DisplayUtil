9970171669aec53dd8c3930ae4579d0db643dc39
Tim Ittermann <tim.ittermann@gmail.com>
2025-06-01 19:47:34 +0200

    feat: add base widget structure

---
+++ DisplayUtil.Home/HomeAssistant/HomeAssistantWidget.cs
@@ -0,0 +1,20 @@
+﻿using DisplayUtil.Widgets.Base;
+using NetDaemon.HassModel;
+
+namespace DisplayUtil.Home.HomeAssistant;
+
+public abstract class HomeAssistantWidget(IHaContext ctx) : ReactiveWidget
+{
+    protected IHaContext Context { get; } = ctx;
+
+    public void OnInitialize()
+    {
+        Subscribe(Context).Subscribe(
+            ActiveObserver.OnNext,
+            ActiveObserver.OnError,
+            ActiveObserver.OnCompleted
+        );
+    }
+
+    protected abstract IObservable<bool> Subscribe(IHaContext context);
+}
---
+++ DisplayUtil.Widgets/Base/ReactiveWidget.cs
@@ -0,0 +1,18 @@
+﻿using System.Reactive.Subjects;
+using DisplayUtil.Layouting;
+
+namespace DisplayUtil.Widgets.Base;
+
+/// <summary>
+///     Helper for widgets that need to be reactive.
+/// </summary>
+public abstract class ReactiveWidget : IWidget
+{
+    private readonly BehaviorSubject<bool> _isActiveSubject = new(false);
+
+    public IObserver<bool> ActiveObserver => _isActiveSubject;
+
+    public bool IsActive => _isActiveSubject.Value;
+
+    public abstract Task<Element> RenderAsync(CancellationToken cancellationToken);
+}
